a228209781abc7420e0e464bf4b40d0718fd2ed0
H143UY <145687297+H143UY@users.noreply.github.com>
2023-09-22 21:09:05 +0700

    first commit

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public int speed;
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        Move_Square();
+        Rotate_Square();
+    }
+    private void Move_Square()
+    {
+        if (this.gameObject.transform.position.x <= 5 && this.gameObject.transform.position.y <=0)
+        {
+            this.gameObject.transform.position += new Vector3(5, 0, 0) * Time.deltaTime * speed;
+        }
+        else if (this.gameObject.transform.position.x >= 5 && this.gameObject.transform.position.y <= 5)
+        {
+            this.gameObject.transform.position += new Vector3(0, 5, 0) * Time.deltaTime * speed;
+        }
+        else if (this.gameObject.transform.position.x >=0  && this.gameObject.transform.position.y < 6 )
+        {
+            this.gameObject.transform.position += new Vector3(-5, 0, 0) * Time.deltaTime * speed;
+            Debug.Log(this.gameObject.transform.position);
+        }
+        else if (this.gameObject.transform.position.x <= 0  && this.gameObject.transform.position.y >= 0)
+        {
+            this.gameObject.transform.position += new Vector3(0, -5, 0) * Time.deltaTime * speed;
+        }
+
+
+    }
+    void Rotate_Square()
+    {
+        if (this.gameObject.transform.position.x == 5 && this.gameObject.transform.position.y == 0)
+        {
+            this.gameObject.transform.Rotate(0, 0, 1);
+        }
+        else if (this.gameObject.transform.position.x == 5 && this.gameObject.transform.position.y == 5)
+        {
+            this.gameObject.transform.Rotate(0, 0, 1);
+        }
+        else if (this.gameObject.transform.position.x == 0 && this.gameObject.transform.position.y == 5)
+        {
+            this.gameObject.transform.Rotate(0, 0, 1);
+        }
+        else if (this.gameObject.transform.position.x == 0 && this.gameObject.transform.position.y == 0)
+        {
+            this.gameObject.transform.Rotate(0, 0, 1);
+        }
+    }
+}
---
+++ Assets/Scripts/tam_giac.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tam_giac : MonoBehaviour
+{
+
+    void Update()
+    {
+        tam_giac_can();
+    }
+    void tam_giac_can()
+    {
+        if(this.gameObject.transform.position.x == 0 && this.transform.position.y == 0)
+        {
+            this.gameObject.transform.position += new Vector3(6, 0, 0);
+        }
+        else if (this.gameObject.transform.position.x == 6 && this.transform.position.y == 0)
+        {
+            this.gameObject.transform.position += new Vector3(-6, 6, 0);
+        }
+    }
+}
